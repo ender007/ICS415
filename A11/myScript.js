9ebb6c6c10d3538632eaabd24be4c548aa12cdbf
ender007 <jwk@hawaii.edu>
2013-09-19 08:46:06 -1000

    A11.2 -Added in javascript to calculate out dates

--- A11/myScript.js
+++ A11/myScript.js
@@ -6,15 +6,16 @@ $(function(){
 	});
 });
 
-/*function daysToDate(month, day){
-	today=new Date()
-    var date=new Date(today.getFullYear(), month, day) //Month is 0-11 in JavaScript
-    if (today.getMonth()==month && today.getDate()>day) //if Christmas has passed already
-         date.setFullYear(date.getFullYear()+1) //calculate next year's Christmas
+function daysToDate(month, day){
+	var today=new Date();
+    var date=new Date(today.getFullYear(), month, day);//Month is 0-11 in JavaScript
+    if (today.getMonth()>month && today.getDate()>day){ //if date has passed already
+         date.setFullYear(date.getFullYear()+1); 
+    }//calculate next year's date
     //Set 1 day in milliseconds
-    var one_day=1000*60*60*24
-
+    var one_day=1000*60*60*24;
+    
     //Calculate difference between the two dates, and convert to days
-    return (Math.ceil((date.getTime()-today.getTime())/(one_day))
-}*/
+    return Math.ceil((date.getTime()-today.getTime())/(one_day));
+}
 
